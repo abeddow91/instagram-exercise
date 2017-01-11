7a21780bb4c0e75ad9a60dcfbccd09acc66868f1
Anna Beddow <anna.beddow@gmail.com>
2017-01-11 18:02:27 +0000

    added carrierwave photo and removed image column

--- instagram/db/schema.rb
+++ instagram/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111172419) do
+ActiveRecord::Schema.define(version: 20170111175856) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170111172419) do
     t.string   "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "photo"
   end
 
 end
