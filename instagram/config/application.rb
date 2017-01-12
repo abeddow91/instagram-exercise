34b28eaa4b1491489c5ff13079995b654252af1b
Anna Beddow <anna.beddow@gmail.com>
2017-01-12 17:38:57 +0000

    added config generators

--- instagram/config/application.rb
+++ instagram/config/application.rb
@@ -11,5 +11,20 @@ module Instagram
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework :rspec,
+        # specifies to  generate a fiture for each model using factory girl factory
+        fixtures: true,
+        # says to skip generting view specs
+        view_specs: false,
+        # generating specs for the helper files Rails generates with each controller.
+        helper_specs: false,
+        # omits a spec file for your config/routes.rb file. If this is a large application, its a god idea to incorporate routing specs
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: => true,
+        # tells Rails to generate factories instead of fixtures and save them in spec/factories directory
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
